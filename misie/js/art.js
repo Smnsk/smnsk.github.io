9cabe8d5aadaa864cdfafb12fb169c11ae4bff9f
Smnsk <artur333@gmail.com>
2017-12-18 11:43:04 +0100

    Create art.js

---
+++ misie/js/art.js
@@ -0,0 +1,66 @@
+var missions = {
+  "year 1":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1994,
+    "date" : ["1.2 - 2.7"],
+    "citys": ["Veľká Lomnica", "Žilina", "Kezmarok"],
+  },
+  "year 2":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1995,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+  "year 3":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1996,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+  "year 4":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1997,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+  "year 5":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1998,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+  "year 6":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 1999,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+  "year 7":{
+    "status": "Misie",
+    "plan" : "Plán misií",
+    "mapa" : "Mapa misií",
+    "year" : 2000,
+    "date" : ["1.2 - 2.7"],
+    "citys" : ["Bratislava"],
+  },
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    show: false,
+    missions: missions
+  }
+})
