e306629096df410d59389059cdcfd21fce9b8305
Smnsk <artur333@gmail.com>
2021-10-25 17:11:25 +0200

    new update

--- web/js/art.js
+++ web/js/art.js
@@ -1,6 +1,6 @@
 let cssr = {
   CSsR: {
-    Eurore: [
+    Europe: [
       {
         loc: "Bratislava-Praha",
         web: "https://redemptoristi.sk/",
@@ -9,67 +9,79 @@ let cssr = {
       },
       {
         loc: "Dublin",
-        web: "",
+        web: "http://www.redemptorists.ie",
         fb: "",
         in: "",
       },
       {
         loc: "Francia",
-        web: "",
+        web: "www.redemptoristes.fr",
         fb: "",
         in: "",
       },
       {
-        loc: "Lisboa",
-        web: "",
+        loc: "Lisbon",
+        web: "http://www.cssr.pt",
         fb: "",
         in: "",
       },
       {
         loc: "London",
-        web: "",
+        web: "http://www.redemptorists.co.uk",
         fb: "",
         in: "",
       },
       {
         loc: "Lviv",
-        web: "",
+        web: "http://www.cssr.lviv.ua",
         fb: "",
         in: "",
       },
       {
         loc: "Madrid",
-        web: "",
+        web: "http://www.redentoristas.org",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Michalovce",
+        web: "http://redemptoristi.nfo.sk",
         fb: "",
         in: "",
       },
       {
         loc: "Napoli",
-        web: "",
+        web: "http://www.redentoristinapoletani.it/",
         fb: "",
         in: "",
       },
       {
         loc: "Roma",
-        web: "",
+        web: "http://www.cssrroma.org",
         fb: "",
         in: "",
       },
       {
-        loc: "Sanctus Clemens",
-        web: "",
+        loc: "St. Clemens",
+        web: "http://www.stclemens.org",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "St. Gerard",
+        web: "http://redemptorist.ru",
         fb: "",
         in: "",
       },
       {
         loc: "Vienna-Monaco",
-        web: "",
+        web: "http://wien-muenchen.redemptoristen.com",
         fb: "",
         in: "",
       },
       {
         loc: "Warszawa",
-        web: "",
+        web: "https://www.redemptor.pl/",
         fb: "",
         in: "",
       },
@@ -77,31 +89,35 @@ let cssr = {
     'America North':[
       {
         loc: "Baltimore",
-        web: "",
+        web: "https://redemptorists.net",
         fb: "",
         in: "",
       },
       {
         loc: "Canada",
-        web: "",
-        fb: "",
+        web: "http://www.redemptoristes.ca",
+        fb: "https://www.facebook.com/Yorkton.Redemptorists",
         in: "",
       },
       {
         loc: "Denver",
-        web: "",
+        web: "http://redemptoristsdenver.org",
         fb: "",
         in: "",
       },
       {
         loc: "Extra Patriam",
-        web: "",
+        web: "http://dccthaingoai.com",
         fb: "",
         in: "",
       },
     ],
   },
-  MSsR:{},
+  MSsR:{
+    Ukraine: {
+
+    }
+  },
   OSsR:{} 
 }
 
@@ -111,11 +127,14 @@ Vue.createApp({
          cssr: cssr,
          show: false,
          show2: false,
+         cur: 0,
+         order: "CSsR"
         }       
      },
   methods:{
-    conti(){
-      this.show2 = !this.show2
+    activeOrden(i, k){
+      this.cur = i
+      this.order = k
     }
 
  }
