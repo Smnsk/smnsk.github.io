3193f464a7fe2e6a50f1b1cabdf5a82100a5a37c
Smnsk <artur333@gmail.com>
2021-10-28 12:39:23 +0200

    new update

--- web/js/art.js
+++ web/js/art.js
@@ -1,5 +1,8 @@
 let cssr = {
   CSsR: {
+    'Africa & Madagascar': [
+      
+    ],
     Europe: [
       {
         loc: "Bratislava-Praha",
@@ -108,6 +111,254 @@ let cssr = {
       {
         loc: "Extra Patriam",
         web: "http://dccthaingoai.com",
+        fb: "",
+        in: "",
+      },
+    ],
+    'America South':[
+      {
+        loc: "Bolivia",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Buenos Aires",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Paraguay",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Perú-Sur",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Resistencia",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Santiago",
+        web: "",
+        fb: "",
+        in: "",
+      },      
+      {
+        loc: "Uruguay",
+        web: "",
+        fb: "",
+        in: "",
+      },
+    ],
+    'Asia & Oceania':[
+      {
+        loc: "Bangalore",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Bangkok",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Cebu",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Colombo",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Giappone",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Indonesia",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Korea",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Liguori",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Majella",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Malaysia & Singapore",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Manila",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Oceania",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "People’s Republic of China",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Vietnam",
+        web: "",
+        fb: "",
+        in: "",
+      },
+    ],
+    'Brazil':[
+      {
+        loc: "Bahia",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Campo Grande",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Fortaleza",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Goiás",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Manaus",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Porto Alegre",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Recife",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Rio de Janeiro",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Sao Paulo",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Suriname",
+        web: "",
+        fb: "",
+        in: "",
+      }
+    ],
+    'Caribbean':[
+      {
+        loc: "Amеrica Central",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Bogota",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Caracas",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Cuba",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Mexica",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Peru North",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Port-au-Prince",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "Quito",
+        web: "",
+        fb: "",
+        in: "",
+      },
+      {
+        loc: "San Juan",
+        web: "",
         fb: "",
         in: "",
       },
