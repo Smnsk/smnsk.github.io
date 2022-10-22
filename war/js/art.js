2f0415f7630e18e144c0c351b453608522dcccd4
Smnsk <artur333@gmail.com>
2022-10-22 16:23:45 +0200

    new update

--- war/js/art.js
+++ war/js/art.js
@@ -3,14 +3,27 @@ let war = [
        name: 'Сабов Руслан Миколайович',
        eng: 'Sabov Ruslan',
        birth: '* 02. 11. 1977',
-       death: '+ 18. 02. 2015'
+       death: '+ 18. 02. 2015',
+       img: 'img/img1.jpg',
+       youtube: 'https://youtu.be/p7UV0PX8_Hs'
     },
     {
-      name: 'Сабо Русла Миколайович',
-      eng: 'Sabo Rusla',
-      birth: '* 02. 11. 1977',
-      death: '+ 18. 02. 2016'
-   },
+       name: 'Лозинський Дмитро Вікторович',
+       eng: 'Lozynskij Dmytro',
+       birth: '* 1986',
+       death: '+ 16 квітня 2022',
+       img: 'img/img2.jpg',
+       youtube: 'https://youtu.be/XsJq0OStJTM'
+    },
+    {
+       name: 'Мурин Михайло Юрійович',
+       eng: 'Muryn Mykhailo',
+       birth: '* 1983',
+       death: '+ 16 квітня 2022',
+       img: 'img/img3.jpg',
+       youtube: 'https://youtu.be/XsJq0OStJTM'
+    },
+  
   ]
 
 const { createApp } = Vue
