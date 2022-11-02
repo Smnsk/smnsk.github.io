57c32c4f4a174a767a04469bb58a172c20b051cf
Smnsk <artur333@gmail.com>
2022-11-02 10:20:31 +0100

    new update

---
+++ catalog/js/art.js
@@ -0,0 +1,47 @@
+let lib = [
+  {
+    titul: "Беркут — птах гірський",
+    name: "Зубанич Ф.І.",
+    code: "545421",
+  },
+  {
+    titul: "Довколосвітня мандрівка у пошуках України",
+    name: "Зубанич Ф.І.",
+    code: "545427",
+  },
+  {
+    titul: "Полонина",
+    name: "Зубанич Ф.І.",
+    code: "545429",
+  },
+  {
+    titul: "",
+    name: "Зубанич Ф.І.",
+    code: "",
+  },
+]
+
+const { createApp } = Vue
+createApp({
+  data() {
+    return {
+      name: "Каталог книг Великоберезнянської центральної бiблiотеки",
+      lib: lib,
+      search: "",
+      place: "",
+    }
+  },
+  computed:{
+    searchFilter(){
+      return this.lib.filter((stat)=>{
+        return stat.titul.match(this.search) + stat.name.match(this.search)
+      })
+  },
+  searchShow(){
+    if(this.search.length > 0){
+      return this.show = true
+   }
+  }
+}
+ 
+}).mount('.lib')
