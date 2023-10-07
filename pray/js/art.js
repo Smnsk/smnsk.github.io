7c557e885e7ef644308e4cbbf9f9a3bdbb824859
smnsk <artur333@gmail.com>
2023-10-07 11:56:39 +0200

    new update

---
+++ pray/js/art.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+    data() {
+         return {
+           titul: "Redemptoristi",
+           prays: prays,
+           
+          }       
+       },
+    methods:{
+   }
+  }).mount('.page')
---
+++ pray/js/db.js
@@ -0,0 +1,30 @@
+let prays = {
+  "Ranná modlitba":{
+    "Pondelok": "a",
+    "Utorok": "b",
+    "Streda": "c",
+    "Štvrtok": "d",
+    "Piatok": "e",
+    "Sobota": "f",
+    "Nedeľa": "g",
+},
+  "Ranné rozjímanie":"",
+  "Modlitby na poludnie":"",
+  "Modlitba pred obedom":{
+    "Pondelok": "a",
+    "Utorok": "b",
+    "Streda": "c",
+    "Štvrtok": "d",
+    "Piatok": "e",
+    "Sobota": "f",
+    "Nedeľa": "g",
+  },
+  "Večerná modlitba":{
+    "Pondelok a Streda": "a",
+    "Utorok a Štvrtok": "b",
+    "Streda a Piatok": "c",
+  },
+  "Návšteva Oltárnej sviatosti":"",
+  "Mariánske Antifony":"",
+  
+}
