ec2dc83bcaedae9818a725792b98dc323b7db706
smnsk <artur333@gmail.com>
2023-11-07 19:54:05 +0100

    new update

--- pray/js/art.js
+++ pray/js/art.js
@@ -1,11 +1,17 @@
-Vue.createApp({
-    data() {
-         return {
-           titul: "Redemptoristi",
-           prays: prays,
-           
-          }       
-       },
-    methods:{
-   }
-  }).mount('.page')
+  let patron = ["Sv. Peter a Sv.Pavol", "Sv. Ondrej", "Sv. Jakub st.","Sv. Ján","Sv. Tomáš","Sv. Jakub ml.","Sv. Filip","Sv. Bartolomej","Sv. Matúš","Sv. Šimon", "Sv. Tadeáš","Sv. Matej"];
+  
+  const date = new Date();
+  let dayWeek = date.getDay();
+  let month = date.getMonth();
+
+  let rm = document.getElementById("rm").children
+  rm[dayWeek + 1].style.display = "block";
+
+  let mj = document.getElementById("mj").children
+  mj[dayWeek + 2].style.display = "block"
+
+  let vm = document.getElementById("vm").children
+   vm[dayWeek + 1].style.display = "block"
+
+let pat = document.getElementById("pat")
+pat.innerHTML = patron[month] 
