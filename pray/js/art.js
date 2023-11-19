c584c05ac470626f33acc1b42e6dfd0b242d59d3
smnsk <artur333@gmail.com>
2023-11-19 16:50:29 +0100

    next update

--- pray/js/art.js
+++ pray/js/art.js
@@ -1,19 +1,156 @@
-  let patron = ["Sv. Peter a Sv.Pavol", "Sv. Ondrej", "Sv. Jakub st.","Sv. Ján","Sv. Tomáš","Sv. Jakub ml.","Sv. Filip","Sv. Bartolomej","Sv. Matúš","Sv. Šimon", "Sv. Tadeáš","Sv. Matej"];
+ let index = {
+  "11-18":[{
+    name: "P. Cyril Jež",
+    year: 1960,
+    loc: "Červenka, okres Olomouc"
+  },
+  { 
+    name: "P. Jiří Kabát",
+    year: 1990,
+    loc: "Příbram"
+  },
+  { 
+    name: "P. Alois Novák",
+    year: 1990,
+    loc: "Opářany, okres Tábor"
+  }
+  ],
+  "11-19":[{
+    name: "P. František Kalina",
+    year: 1929,
+    loc: "Praha-Vyšehrad"
+  },
+  {
+    name: "P. Stanislav Muzikář",
+    year: 2018,
+    loc: "Příbram"
+  },
+],
+  "11-20":[{
+    name: "fr.Epifan (Štefan Daňko)",
+    year: 1982,
+    loc: "Rúbaň, okres Nové Zámky"
+  }
+],
+  "11-21":[{
+    name: "P. František Sal. Alexij Fiala",
+    year: 1992,
+    loc: "Králiky, okres Ústí nad Orlicí"
+  }
+],
+  "11-22":[
+  {
+    name: "fr. Theodosij (Ján Daňko)",
+    year: 1937,
+    loc: "Michalovce"
+  }
+],
+  "11-23":[
+  {
+    name: "P. Ján Gamrát",
+    year: 1991,
+    loc: "Michalovce"
+  }
+],
+  "11-24":[
+  {
+    name: "P. Alfonz Cigáň",
+    year: 1974,
+    loc: "Vřesina, okres Ostrava-město"
+  }
+],
+  "11-26":[
+  {
+    name: "P. Václav Zeman",
+    year: 1973,
+    loc: "České Budějovice"
+  }
+],
+  "11-27":[
+  {
+    name: "P. Jozef Laaber",
+    year: 1933,
+    loc: "Příbram"
+  },
+  {
+    name: "fr. Ján (Michal Novotný)",
+    year: 1943,
+    loc: "Stropkov"
+  }
+],
+  "11-28":[
+  {
+    name: "P. Virgil Čierny",
+    year: 1921,
+    loc: "???"
+  }
+],
+  "11-29":[
+  {
+    name: "fr. Felix Daněček",
+    year: 1947,
+    loc: "Příbram"
+  }
+],
+  "11-30":[
+  {
+    name: "P. Jozef Scholler",
+    year: 1953,
+    loc: "Moravec, Žďar nad Sázavou"
+  },
+  {
+    name: "P. František Nezval",
+    year: 1974,
+    loc: "Brno - Ústřední hřbitov"
+  },
+  {
+    name: "fr. Jan Brůha",
+    year: 1987,
+    loc: "Běšiny, okres Kladno"
+  },
+],
+
+//DECEMBER
+"12-03":[
+  {
+    name: "P. Jozef Čichoň",
+    year: 1997,
+    loc: "Frýdek-Místek"
+  },
+  {
+    name: "Ján Tomaškovič",
+    year: 2009,
+    loc: "Abrahámovce, okres Kežmarok"
+  }
+],
+}
+ 
+ let patron = ["Sv. Peter a Sv.Pavol", "Sv. Ondrej", "Sv. Jakub st.","Sv. Ján","Sv. Tomáš","Sv. Jakub ml.","Sv. Filip","Sv. Bartolomej","Sv. Matúš","Sv. Šimon", "Sv. Tadeáš","Sv. Matej"];
   
-  const date = new Date();
-  let dayWeek = date.getDay();
-  let month = date.getMonth();
+  const date = new Date()
+  let dayWeek = date.getDay()
+  let month = date.getMonth()
+  let day = date.toISOString().slice(5, 10)
 
   let rm = document.getElementById("rm").children
-  rm[dayWeek + 1].style.display = "block";
+  rm[dayWeek + 1].style.display = "block"
 
   let mj = document.getElementById("mj").children
   mj[dayWeek + 2].style.display = "block"
 
   let vm = document.getElementById("vm").children
-   vm[dayWeek + 1].style.display = "block"
 
-let pat = document.getElementById("pat")
-pat.innerHTML = patron[month] 
+  vm[dayWeek >= 4 ? dayWeek - 2 : dayWeek + 1].style.display = "block"
+
+  let firstName = ''
+
+  for(ind of index[day]){
+    
+    firstName += `<div> ${ind.name} (${ind.year}) - ${ind.loc} </div>`
+  }
+  
+  document.getElementById("index").innerHTML = firstName 
+
+  document.getElementById("pat").innerHTML = patron[month] 
 
 
