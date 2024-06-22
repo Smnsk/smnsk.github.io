e0b04597bc2e72d98653c3ba75eced9cb14b053e
Smnsk <artur333@gmail.com>
2024-06-22 11:47:14 +0200

    new update

--- pray/app.js
+++ pray/app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
     if ('serviceWorker' in navigator) {
       try {
-        const reg = await navigator.serviceWorker.register('/sw.js', {scope: '/'})
+        const reg = await navigator.serviceWorker.register('sw.js')
         console.log('SW Register', reg)
       } catch (e) {
         console.log('SW NOT Register ')
